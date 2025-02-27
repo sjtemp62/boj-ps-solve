fd420dc11bd6daf82a641cae5cccf112144c51d8
sjtemp62 <sjtemp62@gmail.com>
2025-02-27 12:12:07 +0900

    1018 코드 이동, 2231 풀이 완료

--- 2231-01-B2.py
+++ 2231-01-B2.py
@@ -1,41 +1,14 @@
-m, n = map(int, input().split())
+num = int(input())
+ans = 1000001
 
-chess_map = list()
+for i in range(num):
+    sum = 0
+    for j in str(i):
+        sum += int(j)
+    if num == i + sum:
+        ans = min(ans, i)
 
-for i in range(m):
-    chess_map.append(input())
-
-min_count = 33
-
-for i in range(m - 8 + 1):
-    for j in range(n - 8 + 1):
-        for BW in ["B", "W"]:
-            count = 0
-            for line in range(8):
-                for block in range(8):
-                    if line % 2 == 0:
-                        if block % 2 == 0:
-                            if chess_map[line + i][block + j] == BW:
-                                continue
-                            else:
-                                count += 1
-                        else:
-                            if chess_map[line + i][block + j] != BW:
-                                continue
-                            else:
-                                count += 1
-                    else:
-                        if block % 2 == 1:
-                            if chess_map[line + i][block + j] == BW:
-                                continue
-                            else:
-                                count += 1
-                        else:
-                            if chess_map[line + i][block + j] != BW:
-                                continue
-                            else:
-                                count += 1
-
-            min_count = min(min_count, count)
-
-print(min_count)
+if ans == 1000001:
+    print(0)
+else:
+    print(ans)
